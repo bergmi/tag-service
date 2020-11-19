91f0374b63fc209167dd115520688cbb9ccd9732
Michael Berg <bergmic1@students.zhaw.ch>
2020-11-19 10:45:34 +0100

    update project, add .env, set port to 3000

--- server.go
+++ server.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"log"
+
 	"github.com/bergmi/tag-service/controllers"
 	"github.com/bergmi/tag-service/models"
 
 	"github.com/gin-gonic/gin"
+	// Import godotenv for .env variables
+	"github.com/joho/godotenv"
 )
 
+// init gets called before the main function
+func init() {
+    // Log error if .env file does not exist
+    if err := godotenv.Load(); err != nil {
+        log.Printf("No .env file found")
+    }
+
+		gin.SetMode(gin.ReleaseMode)
+}
+
 func main() {
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Recovery())
 
 	// Connect to database
 	models.ConnectDatabase()
